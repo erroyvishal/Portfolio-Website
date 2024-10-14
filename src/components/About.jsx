fdc50223889c865e542358b26f88b209996be7fa
erroyvishal <erroyvishal@gmail.com>
2024-10-14 14:16:33 +0530

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Vishal Kumar, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [B.tech/CSE(AIML)], [Technocrats Institute Of Technology(Excellence),Bhopal], [2020 - 2024]
+    
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in C/C++/Python and JavaScript Experienced with Software
+          Tools/Technologies and Strong grasp of Design Principles/Concepts
+          Excellent problem-solving skills Effective communicator and
+          collaborator.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Internship
+        </h1>
+        <span>
+        Cognifyz Technology August                              [ October 2024]
+        <br></br>
+        Website Developer
+        <br></br>
+        Streamlined backend processes by integrating APIs and automating repetitive tasks for improved efficiency.
+        Planned and implemented website from mockups, transforming them into a functional web presence using HTML,
+        <br></br>
+        JavaScript, AJAX, and JSON.
+        Worked closely with UX/UI designers to translate their designs into functional web applications
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+        Earned a 2-star rating on CodeChef by consistently practicing and focusing on enhancing coding skills.
+        <br></br>
+        Completed Accenture Australia Strategy Consulting Job Simulation via Forage, enhancing consulting skills.
+        <br></br>
+        Earned certification in an intensive ReactJS course from Infosys, enhancing front-end development skills.
+        <br></br>
+        Earned a Generative AI course completion certificate from Google, strengthening AI expertise and skills.
+        <br></br>
+        Successfully completed the CyberSecurity course on SkillUp and earned a certificate of achievement.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative Website Developer solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
